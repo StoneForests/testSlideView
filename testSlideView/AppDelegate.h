f2809870ba73e2fa8406275cbe9ad5be2a78c817
yhnbvfrt <yhnbvfrt@wf-iMac.local>
2013-09-10 18:23:28 +0800

    Initial Commit

---
+++ testSlideView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testSlideView
+//
+//  Created by yhnbvfrt on 13-9-10.
+//  Copyright (c) 2013年 yhnbvfrt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
